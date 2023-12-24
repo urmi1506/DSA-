83b77a5b5b998113dfc13b3747b062e95ddeb495
urmi1506 <urmilaawasarmal@gmail.com>
2023-12-24 13:45:47 +0530

    adding function

---
+++ basic operation/hello_world.py
@@ -0,0 +1,8 @@
+def first_function(name):
+    return f"Hello ,{name}"
+
+name = 'urmi'
+print(first_function(name))
+
+# git config --global user.name "urmi1506"
+# git config --global user.email "urmilaawasarmal@gmail.com"
