7bc4dd7320ba0688b8c7fd9bfe6723b85445f91c
Urmila Awasarmal <121875728+urmi1506@users.noreply.github.com>
2024-05-27 18:10:17 +0530

    Update ReverseVowel.java

--- basic-operation/ReverseVowel.java
+++ basic-operation/ReverseVowel.java
@@ -1,16 +1,35 @@
 public class ReverseVowel {
     public static void main(String[] args) {
-       String s="geeksforgeeks";
-       System.out.println(modify (s)); 
+        String s = "abc";
+        System.out.println(modify(s)); 
     }
-    static String modify (String s)
-    {   
-        for (int i = 0; i < s.length(); i--) {
-			if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o'
-					|| s.charAt(i) == 'u') {
-				System.out.print(" " + s.charAt(i));
-			} 
-            if()
+
+    static String modify(String s) {   
+        char[] arr = s.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            while (left < right && !isVowel(arr[left])) {
+                left++;
+            }
+            while (left < right && !isVowel(arr[right])) {
+                right--;
+            }
+            if (left < right) {
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        return new String(arr);
     }
-}
+
+    static boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || 
+               c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
 }
