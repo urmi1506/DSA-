f9e4b66a05b61947d39a6286422252301ec3f917
Urmila Awasarmal <121875728+urmi1506@users.noreply.github.com>
2024-05-27 18:00:51 +0530

    Update LargestEleArr.java

--- basic-operation/LargestEleArr.java
+++ basic-operation/LargestEleArr.java
@@ -1,20 +1,26 @@
 import java.util.ArrayList;
-import java.util.Vector;
 public class LargestEleArr {
     public static void main(String[] args) {
         int n=5;
         int arr[] = {1, 8, 7, 56, 90};
-        
+         
+         ArrayList<Integer> arrayList = new ArrayList<>();
+         for (int i : arr) {
+             arrayList.add(i);
+         }
+ 
+         LargestEleArr e = new LargestEleArr();
+         int largestElement = e.largest(arrayList, n);
+         System.out.println("The largest element in the array is: " + largestElement);
     }
     
     int largest(ArrayList<Integer> arr, int n)
     {
-        int max= arr[0];
-       for (int i = 1; i < n; i++) {
-                if(arr[i]>max)
-                max=arr[i];
-                
-       }
+        int max = arr.get(0); 
+        for (int i = 1; i < n; i++) {
+            if (arr.get(i) > max) 
+                max = arr.get(i);
+        }
        
        return max;
     }
