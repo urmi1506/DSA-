32eb65afa6bff4776f72f665f7a55b9d158a0a4d
urmi1506 <urmilaawasarmal@gmail.com>
2024-07-16 17:31:48 +0530

    Remaining String

---
+++ character_arr_str/RemainingStr.java
@@ -0,0 +1,22 @@
+package character_arr_str;
+public class RemainingStr {
+    public static String printString(String s, char ch, int count) {
+        int cnt2 = 0;
+       
+       for(int i=0;i<s.length();i++){
+           if(s.charAt(i) == ch){
+               cnt2++;
+               if(cnt2 == count){
+                   return s.substring(i+1);
+               }
+           }
+       }
+       return "";
+   }
+   public static void main(String[] args) {
+    String s = "Thisisdemostring";
+    char ch = 'i';
+    int count = 3;
+    System.out.println(printString(s, ch, count));
+   }
+}
