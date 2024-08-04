6069536af0d7dc74facf7676426432f2f62561d6
urmi1506 <urmilaawasarmal@gmail.com>
2024-08-04 23:47:26 +0530

    LeetCode Daily Problem

---
+++ basic-operation/SumSortSubArr.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class SumSortSubArr {
+     public static int rangeSum(int[] nums, int n, int left, int right) {
+         int[] arr = new int[n * (n + 1) / 2];
+        for (int i = 0, k = 0; i < n; ++i) {
+            int s = 0;
+            for (int j = i; j < n; ++j) {
+                s += nums[j];
+                arr[k++] = s;
+            }
+        }
+        Arrays.sort(arr);
+        int ans = 0;
+        final int mod = (int) 1e9 + 7;
+        for (int i = left - 1; i < right; ++i) {
+            ans = (ans + arr[i]) % mod;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+       int [] nums = {1,2,3,4};
+       int  n = 4, left = 1, right = 5;
+       System.out.println(rangeSum(nums, n, left, right));
+    }
+}
