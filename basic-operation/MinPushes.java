8c6244fab4a386be6c8c127f4ae564c0b31fa9cd
urmi1506 <urmilaawasarmal@gmail.com>
2024-08-06 21:32:30 +0530

    LeetCode Daily Problem

---
+++ basic-operation/MinPushes.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class MinPushes {
+
+    public static int minimumPushes(String word) {
+        int[] arr = new int[26];
+        for(int i=0;i<word.length();i++){
+            arr[word.charAt(i) - 'a']++;
+        }
+        Arrays.sort(arr);
+        int i=25;
+        int count = 0;
+        int start = 1;
+        int ans =0;
+        while( i >= 0 && arr[i] != 0){
+            ans+=(start * arr[i]);
+            count++;
+            if(count == 8){ 
+                start++;
+                count = 0;
+            }
+            i--;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        String word = "abcde";
+        System.out.println(minimumPushes(word));
+    }
+}
+
