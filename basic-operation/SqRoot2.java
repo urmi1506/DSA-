cca52ec017648d2c5c238818633efec0b4b1fc5f
urmi1506 <urmilaawasarmal@gmail.com>
2024-08-13 23:30:33 +0530

    GFG Daily Code

---
+++ basic-operation/SqRoot2.java
@@ -0,0 +1,9 @@
+public class SqRoot2 {
+    static long floorSqrt(long n) {
+        return (int)Math.sqrt(n);
+   }
+   public static void main(String[] args) {
+    long n=5;
+    System.out.println(floorSqrt(n) );
+   }
+}
