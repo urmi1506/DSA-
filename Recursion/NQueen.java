52d111424969d6692bc0ad83601f3ddecd94842a
urmi1506 <urmilaawasarmal@gmail.com>
2024-08-30 23:18:48 +0530

    GFG Daily Problem

---
+++ Recursion/NQueen.java
@@ -0,0 +1,66 @@
+
+public class NQueen {
+    public static ArrayList<ArrayList<Integer>> nQueen(int n) {
+        
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = '.';
+
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+
+        int leftRow[] = new int[n];
+        int upperDiagonal[] = new int[2 * n - 1];
+        int lowerDiagonal[] = new int[2 * n - 1];
+
+        solve(0, board, res, leftRow, lowerDiagonal, upperDiagonal);
+
+        Collections.sort(res, (a, b) -> {
+            for (int i = 0; i < a.size(); i++) {
+                if (!a.get(i).equals(b.get(i))) {
+                    return a.get(i) - b.get(i);
+                }
+            }
+            return 0;
+        });
+
+        return res;
+    }
+
+    static void solve(int col, char[][] board, ArrayList<ArrayList<Integer>> res, int leftRow[], int lowerDiagonal[], int upperDiagonal[]) {
+        if (col == board.length) {
+            res.add(construct(board));
+            return;
+        }
+
+        for (int row = 0; row < board.length; row++) {
+            if (leftRow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[board.length - 1 + col - row] == 0) {
+
+                board[row][col] = 'Q';
+                leftRow[row] = 1;
+                lowerDiagonal[row + col] = 1;
+                upperDiagonal[board.length - 1 + col - row] = 1;
+
+                solve(col + 1, board, res, leftRow, lowerDiagonal, upperDiagonal);
+
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiagonal[row + col] = 0;
+                upperDiagonal[board.length - 1 + col - row] = 0;
+            }
+        }
+    }
+
+    static ArrayList<Integer> construct(char[][] board) {
+        ArrayList<Integer> res = new ArrayList<>();
+       
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] == 'Q') {
+                    res.add(j + 1); 
+                }
+            }
+        }
+        return res;
+    }
+}
