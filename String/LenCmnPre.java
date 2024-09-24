37446711cd20a6169e80a7f56fac565184b11311
urmi1506 <urmilaawasarmal@gmail.com>
2024-09-24 16:56:54 +0530

    LeetCode Daily Problem

---
+++ String/LenCmnPre.java
@@ -0,0 +1,41 @@
+package String;
+import java.util.HashMap;
+
+public class LenCmnPre {
+   
+    public static int longestCommonPrefix(int[] arr1, int[] arr2) {
+        HashMap<String, Integer> prefixMap = new HashMap<>();
+        
+        // Step 1: Build the prefix map for arr1
+        for (int num : arr1) {
+            String strNum = Integer.toString(num);
+            String prefix = "";
+            for (char ch : strNum.toCharArray()) {
+                prefix += ch;
+                prefixMap.put(prefix, prefixMap.getOrDefault(prefix, 0) + 1);
+            }
+        }
+        
+        int maxLength = 0;
+        
+        // Step 2: Check for common prefixes in arr2
+        for (int num : arr2) {
+            String strNum = Integer.toString(num);
+            String prefix = "";
+            for (char ch : strNum.toCharArray()) {
+                prefix += ch;
+                if (prefixMap.containsKey(prefix)) {
+                    maxLength = Math.max(maxLength, prefix.length());
+                }
+            }
+        }
+        
+        return maxLength;
+    }
+    public static void main(String[] args) {
+        int []arr1 = {1,10,100}; 
+        int[]arr2 = {1000};
+        System.out.println(longestCommonPrefix(arr1, arr2) );
+    }
+}
+
