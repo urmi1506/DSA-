6843a00a4b573c1c14baa446889f56bd781c885a
urmi1506 <urmilaawasarmal@gmail.com>
2024-11-17 19:09:22 +0530

    LeetCode Daily Problem

---
+++ basic-operation/ShortestSubarr.java
@@ -0,0 +1,31 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+class ShortestSubarr {
+    public int shortestSubarray(int[] nums, int k) {
+        int n = nums.length;
+        long[] prefix = new long[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+
+        Deque<Integer> deque = new LinkedList<>();
+        int minLen = Integer.MAX_VALUE;
+
+        for (int i = 0; i <= n; i++) {
+            while (!deque.isEmpty() && prefix[i] - prefix[deque.peekFirst()] >= k) {
+                minLen = Math.min(minLen, i - deque.pollFirst());
+            }
+
+            while (!deque.isEmpty() && prefix[i] <= prefix[deque.peekLast()]) {
+                deque.pollLast();
+            }
+
+            deque.addLast(i);
+        }
+
+        return minLen == Integer.MAX_VALUE ? -1 : minLen;
+    }
+}
+
