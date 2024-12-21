d02bc62dae63e283cbc722d93fc33dd2b3121d68
urmi1506 <urmilaawasarmal@gmail.com>
2024-12-21 16:27:42 +0530

    LinkedList Binary Tree

--- LinkedList/LLBinaryTree.java
+++ LinkedList/LLBinaryTree.java
@@ -1,29 +1,28 @@
 package LinkedList;
 
-public  class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
-
-public  class TreeNode {
-   
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-       
-    }
 public class LLBinaryTree {
-
+    public  static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    
+    public static class TreeNode {
+       
+            int val;
+            TreeNode left;
+            TreeNode right;
+            TreeNode() {}
+            TreeNode(int val) { this.val = val; }
+            TreeNode(int val, TreeNode left, TreeNode right) {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+           
+        }
     public static boolean isSubPath(ListNode head, TreeNode root) {
         return dfs(head, head, root);
     }
