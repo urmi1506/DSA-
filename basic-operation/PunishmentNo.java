6cff5e7853ddd92f591fdd9e2acc13791bd312cc
urmi1506 <urmilaawasarmal@gmail.com>
2025-02-15 16:38:52 +0530

    LeetCode Daily Problem Sol

---
+++ basic-operation/PunishmentNo.java
@@ -0,0 +1,16 @@
+public class PunishmentNo {
+        public static int punishmentNumber(int n) {
+            int[] arr = {1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,657,675,703,756,792,909,918,945,964,990,991,999,1000};
+            int sum = 0;
+            for(int i=0;i<arr.length;i++){
+                if(arr[i]<=n) sum += arr[i]*arr[i];
+                else if(arr[i]>n) break;
+            }
+            return sum;
+        }
+    public static void main(String[] args) {
+        int n=10;
+        System.out.println(punishmentNumber(n) );
+        
+    }
+}
