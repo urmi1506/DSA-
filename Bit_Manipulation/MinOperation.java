94c03960fbe3928dee42381ae50206a6afe6caea
urmi1506 <urmilaawasarmal@gmail.com>
2025-03-20 00:00:38 +0530

    Daily Problem Sol Of LeetCode

---
+++ Bit_Manipulation/MinOperation.java
@@ -0,0 +1,23 @@
+package Bit_Manipulation;
+
+public class MinOperation {
+        public static int minOperations(int[] nums) {
+            int count = 0;
+            int n = nums.length;
+    
+            for (int i = 0; i < n - 2; i++) {
+                if (nums[i] == 0) {
+                    nums[i] ^= 1;
+                    nums[i + 1] ^= 1;
+                    nums[i + 2] ^= 1;
+                    count++;
+                }
+            }
+    
+            return (nums[n - 2] == 1 && nums[n - 1] == 1) ? count : -1;
+        }
+    public static void main(String[] args) {
+        int[] nums = {0,1,1,1,0,0};
+        System.out.println(minOperations(nums));
+    }
+}
