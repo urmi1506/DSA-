9c9f569835984d32ddce3f76b1ec763b54476b9c
urmi1506 <urmilaawasarmal@gmail.com>
2025-06-04 15:10:46 +0530

    Rotated and Sorted Array

---
+++ Array/LeftRotateArrK.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class LeftRotateArrK {
+    public static void LeftRotateByK(int[] arr,int n,int k){
+        int x = arr[k-2];
+        for (int i = 0; i < n-k; i++) {
+            arr[i] = arr[i+k];
+        }
+        arr[n-k] = x;
+        for (int i = 0; i < n ; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        int n=6;
+        int arr[]={1, 2, 3, 4, 5, 6};
+        int k =2;
+        LeftRotateByK(arr, n, k);
+    }
+}
