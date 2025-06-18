6162de2c428a3b43318585a852c48f09c3d2e874
urmi1506 <urmilaawasarmal@gmail.com>
2025-06-18 17:21:50 +0530

    Optimized Sol ,TwoSum

---
+++ Array/Medium/TwoSum2.java
@@ -0,0 +1,34 @@
+package Medium;
+
+import java.util.HashMap;
+
+public class TwoSum2 {
+    public static int[] twoSum(int[]nums ,int target){
+
+        // Array to store ans
+        int ans[] = new int[2];
+        ans[0] =ans[1] = -1;
+        // hashmap 
+        HashMap <Integer,Integer> map =new HashMap<>();
+        for (int i = 0; i < ans.length; i++) {
+            int num =nums[i];
+            // cal MoreNeeded To reach target
+            int moreNeeded = target - num;
+            if (map.containsKey(moreNeeded)){
+                ans[0] =map.get(moreNeeded);
+                ans[1] =i;
+            return ans;
+            }
+            map.put(nums[i],i);
+        }
+    return ans;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9 ;
+        int[]res =twoSum(nums, target);
+        for (int i : res) {
+            System.out.println(i);
+        }
+    }
+}
