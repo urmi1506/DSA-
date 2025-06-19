9373861367831b8c0c6a923e550e19d91f800799
urmi1506 <urmilaawasarmal@gmail.com>
2025-06-19 18:57:46 +0530

    Kadane Algo ,Brute-Force

---
+++ Array/Medium/MaxSubArr2.java
@@ -0,0 +1,20 @@
+package Medium;
+
+public class MaxSubArr2 {
+    public static int maxSubArray(int[] nums) {
+       int n = nums.length;
+
+       int sum = 0;
+       int maxi = Integer.MIN_VALUE;
+
+       for (int i = 0; i < n; i++) {
+           sum += nums[i];
+           maxi =Math.max(maxi, sum);
+       }
+    return maxi;
+    }
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
+    }
+}
