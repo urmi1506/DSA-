9bc4d89447a49e7b51bf48537c808696b7e4bac4
urmi1506 <urmilaawasarmal@gmail.com>
2025-06-19 16:05:29 +0530

    Updated Sol

--- Array/Medium/MaxEle2.java
+++ Array/Medium/MaxEle2.java
@@ -1,22 +1,12 @@
 package Medium;
 
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class MaxEle2 {
     public static int majorityElement(int[] nums) {
+        Arrays.sort(nums);
         int n = nums.length;
-        int maxCnt=0;
-        int cnt =1; 
-        int majorityEle=nums[0];
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            if(map.containsKey(majorityEle)) cnt ++;
-            if(cnt > maxCnt){
-                maxCnt = cnt ;
-                majorityEle=nums[i];
-            }
-        }
-    return majorityEle;
+        return nums[n/2];
     }
      public static void main(String[] args) {
         int[]nums = {2,2,1,1,1,2,2};
