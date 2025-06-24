a83e7b4a8d94620ccba8dc6f902929ae268c3bf2
urmi1506 <urmilaawasarmal@gmail.com>
2025-06-24 19:25:40 +0530

    New Changes

--- Array/Medium/SubArrSumK.java
+++ Array/Medium/SubArrSumK.java
@@ -14,7 +14,7 @@ public class SubArrSumK {
         }
     return cnt;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) {    
         int[] nums ={1,1,1};
         int k =2;
         System.out.println(subarraySum(nums, k));
