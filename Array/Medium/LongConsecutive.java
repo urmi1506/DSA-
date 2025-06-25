0a2452e4dd4eaa551b0868dec2d64631f4e0f4e9
urmi1506 <urmilaawasarmal@gmail.com>
2025-06-25 19:16:42 +0530

    Brute-Force ,Longest-Consecutive

---
+++ Array/Medium/LongConsecutive.java
@@ -0,0 +1,27 @@
+package Medium;
+
+import java.util.Arrays;
+
+public class LongConsecutive {
+    public static int longestConsecutive(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int res = 1;
+        int cnt =1;
+
+        for (int i = 1; i < n; i++) {
+            if(nums[i] == nums[i-1]) continue;
+            if(nums[i] == nums[i-1]+1) cnt ++;
+            else cnt =1;
+
+            res=Math.max(res,cnt);
+
+        }
+    return res;
+        
+    }
+    public static void main(String[] args) {
+        int []nums={100, 4, 200, 1, 3, 2};
+        System.out.println(longestConsecutive(nums));
+    }
+}
