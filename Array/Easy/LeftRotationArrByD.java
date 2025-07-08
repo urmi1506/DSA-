4dbda3a85082d3e3b38a04837380abf4d43dac36
urmi1506 <urmilaawasarmal@gmail.com>
2025-07-08 22:50:35 +0530

    new changes

--- Array/Easy/LeftRotationArrByD.java
+++ Array/Easy/LeftRotationArrByD.java
@@ -20,8 +20,8 @@ public class LeftRotationArrByD{
         reverse(nums, 0, n-1);
     }
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        int k = 3;
+        int[] nums = {1, 2, 3, 4, 5};
+        int k = 2;
 
         rotate(nums, k);
 
