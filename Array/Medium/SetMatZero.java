a9047a632d5545283b85960ddb4e5e033da32b8a
urmi1506 <urmilaawasarmal@gmail.com>
2025-07-17 22:38:06 +0530

    Brute-Force,SetMatrixZero

---
+++ Array/Medium/SetMatZero.java
@@ -0,0 +1,49 @@
+package Medium;
+
+public class SetMatZero {
+    public static void setZeroes(int[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for(int row=0; row<n; row++){
+            for(int col=0; col<m; col++){
+                if(matrix[row][col]==0){
+                markRow(matrix,row);
+                markCol(matrix,col);
+                }
+            }
+        }
+    convert(matrix);
+    }
+    public static void markRow(int[][]matrix,int row){
+        int m=matrix[0].length;
+        for(int col=0; col<m; col++){
+            if(matrix[row][col] !=0) matrix[row][col]=-1;
+        }
+    }
+    public static void markCol(int[][]matrix,int col){
+        int n=matrix.length;
+        for(int row=0; row<n; row++){
+            if(matrix[row][col]!=0) matrix[row][col]=-1;
+        }
+    }
+    public static void convert(int[][]matrix){
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for(int row=0; row<n; row++){
+            for(int col=0; col<m; col++){
+                if(matrix[row][col]==-1) matrix[row][col]=0;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int matrix[][]={{1,1,1},{1,0,1},{1,1,1}};
+        setZeroes(matrix);
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
