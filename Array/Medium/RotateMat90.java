1759387043ef412139738bb48a51df305097d12e
urmi1506 <urmilaawasarmal@gmail.com>
2025-07-18 20:45:28 +0530

    Optimal-sol , Rotate Matrix 90

---
+++ Array/Medium/RotateMat90.java
@@ -0,0 +1,31 @@
+package Medium;
+
+public class RotateMat90 {
+     public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+               int temp=matrix[i][j];
+               matrix[i][j]=matrix[j][i];
+               matrix[j][i]=temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n/2; j++) {
+               int temp=matrix[i][j];
+               matrix[i][j]=matrix[i][n-1-j];
+               matrix[i][n-1-j]=temp;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int mat[][]={{1,2},{3,4}};
+
+        for (int[] row : mat) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
