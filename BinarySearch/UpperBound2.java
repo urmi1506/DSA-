f7e53b96458e22219fa189b71e2e00fb921afed8
urmi1506 <urmilaawasarmal@gmail.com>
2025-07-23 19:48:03 +0530

    Optimized-UpperBound

--- BinarySearch/UpperBound2.java
+++ BinarySearch/UpperBound2.java
@@ -7,17 +7,19 @@ public class UpperBound2 {
          int high=n-1;
          int ans=n;
          while(low<=high){
-            int mid=high+low/2;
-            if(mid >x){
+            int mid=low+(high-low)/2;
+            if(nums[mid] >x){
                 ans= mid;
                 high=mid-1;
             }
+            else{
             low=mid+1;
+            }
          }
        return ans;
      }
      public static void main(String[] args) {
-        int nums[]={1,2,2,3};
+        int nums[]={3,5,8,15,19};
         int x=2;
         System.out.println(upperBound(nums, x));
     }
