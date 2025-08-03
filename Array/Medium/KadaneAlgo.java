925e74f51bf4fe965b55cfb67b960dce8b7915a4
urmi1506 <urmilaawasarmal@gmail.com>
2025-08-03 22:05:53 +0530

    Optimized-sol ,Kadanes Algo

---
+++ Array/Medium/KadaneAlgo.java
@@ -0,0 +1,18 @@
+package Medium;
+public class KadaneAlgo {
+    public static int maxSum(int[]nums){
+        int n=nums.length;
+        int sum =0;
+        int maxi=Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            sum+=nums[i];
+            if (maxi >sum) sum=maxi;
+            if (sum <0) sum=0;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int nums[]={-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSum(nums));
+    }
+}
