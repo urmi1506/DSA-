07be85b0c5c3ee73e9cb9977be4cb52ab1eaa341
urmi1506 <urmilaawasarmal@gmail.com>
2025-08-04 16:58:16 +0530

    Optimized-sol , Peak Element

--- BinarySearch/PeakEle.java
+++ BinarySearch/PeakEle.java
@@ -3,6 +3,7 @@ package BinarySearch;
 public class PeakEle{
     public static int findPeakElement(int[] nums) {
         int n=nums.length;
+        
         if(n==1) return 0;
         if(nums[0]>nums[1]) return 0;
         if(nums[n-1]>nums[n-2]) return n-1;
