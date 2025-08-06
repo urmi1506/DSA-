b8184b745d62d3733950810cb4ab399f3ec00f0e
urmi1506 <urmilaawasarmal@gmail.com>
2025-08-06 12:56:32 +0530

    Optimized-Sol,..SearchMatrix

---
+++ BinarySearch/MatrixSearch.java
@@ -0,0 +1,30 @@
+package BinarySearch;
+public class MatrixSearch {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int low = 0;
+        int high = rows * cols - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int midVal = matrix[mid / cols][mid % cols]; 
+
+            if (midVal == target) {
+                return true;
+            } else if (midVal < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+    public static void main(String[] args) {
+        int[][]matrix={{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        int target=3;
+        System.out.println(searchMatrix(matrix, target));
+    }
+}
