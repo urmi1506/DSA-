0ff8fa4c63de585b507b5f309185acb70d0f89ad
urmi1506 <urmilaawasarmal@gmail.com>
2025-08-07 21:22:16 +0530

    Revise Bubble Sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -12,7 +12,7 @@ public class BubbleSort {
         // traverse passes from 1 to n-1....traversing whole array  we canalso take i=1 i<n 
     
         for (int i = 0; i < n-1; i++) {
-        // for swap element & take n-1-i bcz every time we sorted the largest element and after that their is no need to sort that element j=0 j<n-i
+        // for swap element & take n-1-i bcz every time we sorted the largest element and after that their is no need to sort that element j=0 j<n-i-1
             for (int j = 0; j < n-1-i; j++) {
                if(arr[j]>arr[j+1]) {
                 int temp =arr[j];
