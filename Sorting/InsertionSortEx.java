ae3a0d90b19e75d21507232e3f794e5f782925f3
urmi1506 <urmilaawasarmal@gmail.com>
2025-08-09 15:05:34 +0530

    InsertionSort

--- Sorting/InsertionSortEx.java
+++ Sorting/InsertionSortEx.java
@@ -2,26 +2,28 @@ package Sorting;
 
 public class InsertionSortEx {
     public static void insertionSort(int []arr){
-        int n=arr.length;
-        for (int i = 1; i < n; i++) {
-            // peak element to place it in correct pos
+        int n= arr.length;
+        // Assume 1st element is sorted
+        for(int i=1; i<n; i++){
+            // peak element
             int temp=arr[i];
             int j=i-1;
-            // shift larger element to right
+            // shift larger element at right ..compare peak ele with curr ele
             while(j>=0 && arr[j]>temp){
                 arr[j+1]=arr[j];
                 j--;
             }
-            // place peak element at correct postion
+            // place element at correct pos
             arr[j+1]=temp;
-
         }
+
     }
     public static void main(String[] args) {
-        int []arr={4, 1, 3, 9, 7};
+        int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+		int n = arr.length;
         insertionSort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        for (int i : arr) {
+            System.out.println(i);
         }
     }
 }
