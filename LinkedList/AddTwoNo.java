3c1be62cca8f0494442038510d3761f04381b77c
urmi1506 <urmilaawasarmal@gmail.com>
2025-08-19 13:17:30 +0530

    AddTwoNumber

---
+++ LinkedList/AddTwoNo.java
@@ -0,0 +1,66 @@
+package LinkedList;
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+// GM
+
+public class AddTwoNo {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int val1 = (l1 != null) ? l1.val : 0; 
+            int val2 = (l2 != null) ? l2.val : 0;
+
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;  
+            int digit = sum % 10; 
+
+            current.next = new ListNode(digit); 
+            current = current.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        if (carry > 0) {
+            current.next = new ListNode(carry);
+        }
+
+        return dummy.next; 
+    }
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val);
+            if (node.next != null) System.out.print(" -> ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode result = addTwoNumbers(l1, l2);
+        printList(result);  
+    }
+
+
+}
+ 
