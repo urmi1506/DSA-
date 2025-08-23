78313ea687e1e78437e559c080f998f5a3119d9c
urmi1506 <urmilaawasarmal@gmail.com>
2025-08-23 13:14:27 +0530

    optimized-sol,Rotate90

--- Array/Medium/Rotate90.java
+++ Array/Medium/Rotate90.java
@@ -1,24 +1,26 @@
 package Medium;
 
 public class Rotate90 {
-    public void rotate(int[][] matrix) {
+    public static void rotate(int[][] matrix) {
         int n=matrix.length;
-        int temp[][]=new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                temp[i][j]=matrix[j][n-i-1];
+
+        int ans[][]=new int[n][n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                ans[j][n-i-1]=matrix[i][j];
             }
         }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                matrix[i][j]=temp[i][j];
+                matrix[i][j] = ans[i][j];
             }
         }
+        
     }
    
 public static void main(String[] args) {
-        int mat[][]={{1,2},{3,4}};
-
+        int mat[][]={{1,2,3},{4,5,6},{7,8,9}};
+        rotate(mat);
         for (int[] row : mat) {
             for (int num : row) {
                 System.out.print(num + " ");
