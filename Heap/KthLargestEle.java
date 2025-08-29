80ae956f4d9e6d5d163f7e13051848c84806ae69
urmi1506 <urmilaawasarmal@gmail.com>
2025-08-29 14:48:18 +0530

    Better-sol,KthLargestElement

---
+++ Heap/KthLargestEle.java
@@ -0,0 +1,23 @@
+package Heap;
+
+import java.util.PriorityQueue;
+
+public class KthLargestEle {
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            minHeap.add(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        return minHeap.peek();        
+    }
+    public static void main(String[] args) {
+        int nums[]={3,2,3,1,2,4,5,5,6};
+        int k=4;
+        System.out.println(findKthLargest(nums, k));
+    }
+}
