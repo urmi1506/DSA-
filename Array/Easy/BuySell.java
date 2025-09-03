a65c60200bf307905f716e3948845aa559a7837b
urmi1506 <urmilaawasarmal@gmail.com>
2025-09-03 18:52:51 +0530

    Optimized_sol, Buy_Sell

---
+++ Array/Easy/BuySell.java
@@ -0,0 +1,22 @@
+package Easy;
+
+public class BuySell {
+    public static int maxProfit(int[] prices) {
+        // sell price must always come after buy price (hence not update sell otherwise its not match above cond)
+        int n=prices.length;
+        // buy ...when value is smallest
+        int buy=prices[0];
+        // profit
+        int profit=0;
+        
+        for(int i=1; i<n; i++){
+            if(prices[i]<buy) buy=prices[i];
+            profit=Math.max(profit,prices[i]-buy);
+        }
+        return profit;
+    }
+    public static void main(String[] args) {
+        int prices[]={7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+}
