e4b1993b3124f058f065a8236df1b7062901d92b
urmi1506 <urmilaawasarmal@gmail.com>
2025-09-10 23:17:23 +0530

    optimized-sol,kLargestElements

---
+++ Heap/kLargestElements.java
@@ -0,0 +1,25 @@
+package Heap;
+
+import java.util.PriorityQueue;
+
+public class kLargestElements {
+    public static void kLargest(int[] nums ,int k){
+        PriorityQueue<Integer> minHeap=new PriorityQueue<>();
+        for(int num:nums){
+            minHeap.add(num);
+            if(minHeap.size()>k){
+                minHeap.poll();
+            }
+        }
+        while (minHeap.size()>0) {
+            System.out.print(minHeap.poll()+" ");
+            
+        }
+    }
+    public static void main(String[] args) {
+        int nums[]={3,2,1,5,6,4};
+        int k=2;
+        kLargest(nums, k);
+        
+    }
+}
