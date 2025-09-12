bb80d714543fae7ab3f5d0b814ad573ebb755c72
urmi1506 <urmilaawasarmal@gmail.com>
2025-09-12 19:04:35 +0530

    Kadanes Algorithm

--- Array/Medium/KadaneAlgo.java
+++ Array/Medium/KadaneAlgo.java
@@ -9,7 +9,7 @@ public class KadaneAlgo {
             if (sum > maxi) maxi=sum;
             if (sum <0) sum=0;
         }
-        return sum;
+        return maxi;
     }
     public static void main(String[] args) {
         int nums[]={-2,1,-3,4,-1,2,1,-5,4};
