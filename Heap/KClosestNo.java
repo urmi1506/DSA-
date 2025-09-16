af3ae90a0ddacd89eb59eb8c9327d7532d8e1b7b
urmi1506 <urmilaawasarmal@gmail.com>
2025-09-16 19:59:41 +0530

    optimized-sol ,KClosestNo

---
+++ Heap/KClosestNo.java
@@ -0,0 +1,25 @@
+package Heap;
+
+import java.util.PriorityQueue;
+
+public class KClosestNo {
+    public static void kClosestno(int[] nums , int k , int x){
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        for (int num : nums) {
+            maxHeap.add(new int[]{Math.abs(num - x), num});
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+        while (!maxHeap.isEmpty()) {
+            System.out.print(maxHeap.poll()[1] + " ");
+            
+        }
+    }
+    public static void main(String[] args) {
+        int [] nums = {5,6,7,8,9};
+        int k = 3;
+        int x = 7;
+        kClosestno(nums,k,x);
+    }
+}
