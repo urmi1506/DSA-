d0e0fae6e93c3c287da90983cfa8438cc32cf659
urmi1506 <urmilaawasarmal@gmail.com>
2025-09-19 23:11:57 +0530

    optimized-sol ,MaxConsecutive1's

--- Array/Easy/MaxConsecutive.java
+++ Array/Easy/MaxConsecutive.java
@@ -4,7 +4,6 @@ public class MaxConsecutive {
     public static int maxConsecutives(int[] nums ){
         int n = nums.length;
 
-        // track the count of 1
         int cnt = 0;
         // track max cnt we want to return max cnt
         int maxi = 0;
