68910e425432b2f98eeec0faa9f82073286d9883
urmi1506 <urmilaawasarmal@gmail.com>
2025-10-03 22:54:31 +0530

    Better-sol ,Right_view_BinaryTree

--- Trees/RightViewTree.java
+++ Trees/RightViewTree.java
@@ -2,41 +2,39 @@ package Trees;
 
 import java.util.ArrayList;
 
-
 public class RightViewTree {
-    static class Node
-{
-    int data;
-    Node left, right;
+    static class Node {
+        int data;
+        Node left, right;
 
-    Node(int item)
-    {
-        data = item;
-        left = right = null;
+        Node(int item) {
+            data = item;
+            left = right = null;
+        }
     }
-}
-      static ArrayList<Integer> rightView(Node root)
-    {
-      ArrayList<Integer> res=new ArrayList<Integer>();
-      rightViewHelper(root,res,0);
-      return res;
+
+    public static ArrayList<Integer> rightView(Node root) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        rightViewHelper(root, res, 0);
+        return res;
     }
-    static void rightViewHelper(Node currNode, ArrayList<Integer>res, int currDepth){
-        if(currNode==null)return;
-        
-        if(currDepth==res.size()){
+
+    private static void rightViewHelper(Node currNode, ArrayList<Integer> res, int currDepth) {
+        if (currNode == null)
+            return;
+
+        if (currDepth == res.size()) {
             res.add(currNode.data);
         }
-        
-        rightViewHelper(currNode.right,res,currDepth+1);
-        rightViewHelper(currNode.left,res,currDepth+1);
+
+        rightViewHelper(currNode.right, res, currDepth + 1);
+        rightViewHelper(currNode.left, res, currDepth + 1);
     }
+
     public static void main(String[] args) {
         Node root = new Node(1);
-        root.left=new Node(3);
-        root.right=new Node(2);
-        System.out.println(rightView( root));
+        root.left = new Node(3);
+        root.right = new Node(2);
+        System.out.println(rightView(root));
     }
 }
-
-
