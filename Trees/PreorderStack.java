84ef3dbaf6d2fa512ce88353a48066fda94b5706
urmi1506 <urmilaawasarmal@gmail.com>
2025-11-04 13:25:05 +0530

    Preorder using Stack

--- Trees/PreorderStack.java
+++ Trees/PreorderStack.java
@@ -9,32 +9,29 @@ public class PreorderStack {
         int data;
         TreeNode left;
         TreeNode right;
-        TreeNode() {
-            this.data = 0;
-            this.left = null;
-            this.right = null;
-        }
+        
         TreeNode(int data) {
             this.data = data;
             this.left = null;
             this.right = null;
         }
-        TreeNode(int data, TreeNode left, TreeNode right) {
-            this.data = data;
-            this.left = left;
-            this.right = right;
-        }
+        
     }
     public static List<Integer> preorderTraversal(TreeNode root){
        List<Integer>preorder =new ArrayList<Integer>();
-       if (root==null) return preorder;
        Stack<TreeNode> stack=new Stack<TreeNode>();
+    //Base case
+       if (root==null) return preorder;
+
        stack.push(root);
+    // check Preorder
        while(!stack.isEmpty()){
         root =stack.peek();
         stack.pop();
         preorder.add(root.data);
+        // check right
         if(root.right != null) stack.push(root.right);
+        // check left
         if(root.left != null) stack.push(root.left);
        }
     return preorder;
@@ -47,8 +44,8 @@ public class PreorderStack {
         root.left.left = tree.new TreeNode(4);
         root.left.right = tree.new TreeNode(5);
 
-        List<Integer> preOrderResult = preorderTraversal(root);
+        List<Integer> Res = preorderTraversal(root);
 
-        System.out.println(  preOrderResult);
+        System.out.println(Res);
     }
 }
