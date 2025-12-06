c2af098dc3ee818f30b1b9f1f6702856c1ca2ae6
urmi1506 <urmilaawasarmal@gmail.com>
2025-12-06 22:31:50 +0530

    Middle of LinkedList ...Brute-Force

--- LinkedList/MiddleLL.java
+++ LinkedList/MiddleLL.java
@@ -1,62 +1,43 @@
 package LinkedList;
 
+import java.util.ArrayList;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
 public class MiddleLL {
+    public static ListNode middleNode(ListNode head) {
+        ArrayList<ListNode> list = new ArrayList<>();
+        ListNode curr = head;
+
+        // 1.store in temp arraylist
+        while (curr != null) {
+            list.add(curr);
+            curr = curr.next;
+        }
+        // 2.find middle
+        int n = list.size();
+        return list.get(n / 2);
+    }
 
     public static void main(String[] args) {
-        int[] values = {1, 2, 3, 4, 5};
-        MiddleLL list = new MiddleLL();
-        Node head = list.constructLL(values);
-        
-        Solution solution = list.new Solution();
-        int middleValue = solution.getMiddle(head);
-        
-        System.out.println("Middle element: " + middleValue);
-    }
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
 
-    class Node {
-        int data;
-        Node next;
-
-        Node(int d) {
-            data = d;
-            next = null;
-        }
-    }
-
-    Node constructLL(int[] values) {
-        if (values.length == 0) {
-            return null;
-        }
-        Node head = new Node(values[0]);
-        Node temp = head;
-
-        for (int i = 1; i < values.length; i++) {
-            temp.next = new Node(values[i]);
-            temp = temp.next;
-        }
-
-        return head;
-    }
-
-    void printLinkedList(Node head) {
-        Node temp = head;
+        ListNode temp = middleNode(head);
         while (temp != null) {
-            System.out.print(temp.data + " ");
+            System.out.print(temp.val + "-->");
             temp = temp.next;
-        }
-        System.out.println();
-    }
-
-    class Solution {
-        int getMiddle(Node head) {
-            Node slow = head;
-            Node fast = head;
-
-            while (fast != null && fast.next != null) {
-                slow = slow.next;
-                fast = fast.next.next;
-            }
-            return slow.data;
         }
     }
 }
