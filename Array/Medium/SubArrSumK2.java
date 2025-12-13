da2d5d457625cc79837d2fac7853d93a5d6ea3ac
urmi1506 <urmilaawasarmal@gmail.com>
2025-12-13 23:03:36 +0530

    SubArraySumK....optimized-sol

--- Array/Medium/SubArrSumK2.java
+++ Array/Medium/SubArrSumK2.java
@@ -7,13 +7,15 @@ public class SubArrSumK2 {
         int n = nums.length;
         int cnt = 0;
         int sum = 0;
-
+        //store prefix sum & its freq
         HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);  
-
+       
+        //Traverse arr
         for (int i = 0; i < n; i++) {
+            //curr sum
             sum += nums[i];
-
+            //check sum-k present or not
             if (map.containsKey(sum - k)) {
                 cnt += map.get(sum - k);
             }
