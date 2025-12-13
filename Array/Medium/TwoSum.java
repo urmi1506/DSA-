a6dbe07a2d26eb8b1d88827874c3a4600da45e9c
urmi1506 <urmilaawasarmal@gmail.com>
2025-12-13 20:58:15 +0530

    Two-Sum ...Brute-force-sol

--- Array/Medium/TwoSum.java
+++ Array/Medium/TwoSum.java
@@ -8,9 +8,10 @@ public class TwoSum{
         // initially both indx -1
         ans[0]= ans[1]=-1;
 
-       // Find Two Sum Of Array    
+       // Find Two Sum Of Arr   
         for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j < nums.length; j++) {
+
                 if(nums[i] + nums[j] == target) {
                     ans[0] =i;
                     ans[1] =j;
