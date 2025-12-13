55c29977931852c361b4e640cd63cda7b5279c8f
urmi1506 <urmilaawasarmal@gmail.com>
2025-12-13 22:54:52 +0530

    SubArraySumK ...Brute-force-sol

--- Array/Medium/SubArrSumK.java
+++ Array/Medium/SubArrSumK.java
@@ -4,11 +4,14 @@ public class SubArrSumK {
     public static int subarraySum(int[] nums, int k) {
         int n = nums.length;
         int cnt =0;
-
+        //Traverse arr
         for (int i = 0; i < n; i++) {
             int sum =0;
+            //Expand subarr
             for (int j = i; j < n; j++) {
+                //compute sum
                 sum += nums[j];
+                //track cnt
                 if(sum == k ) cnt ++;
             }
         }
