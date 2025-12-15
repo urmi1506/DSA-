a7c8272b60f975a8fe77f2594309925762626228
urmi1506 <urmilaawasarmal@gmail.com>
2025-12-15 22:36:54 +0530

    Missing_No ...optimized-sol

--- Array/Easy/MissingNo2.java
+++ Array/Easy/MissingNo2.java
@@ -5,17 +5,17 @@ public class MissingNo2 {
          int n = nums.length ;
         int xor1 = 0, xor2 = 0;
 
-        // XOR all array elements
+        // XOR all arr ele
         for (int i = 0; i < n ; i++) {
             xor2 ^= nums[i];
         }
 
-        // XOR all numbers from 1 to n
+        // XOR all no from 1 to n
         for (int i = 0; i <= n; i++) {
             xor1 ^= i;
         }
 
-        // Missing number is the XOR of xor1 and xor2
+        // Missing no = xor1 ^ xor2
         return xor1 ^ xor2;
     }
     public static void main(String[] args) {
