8d0374782408a65c5489346d573df74b8df3a0d2
urmi1506 <urmilaawasarmal@gmail.com>
2025-12-23 22:51:51 +0530

    Diameter of Binary Tree ---Optimized-sol

--- Trees/DiameterBT.java
+++ Trees/DiameterBT.java
@@ -1,48 +1,57 @@
 package Trees;
 
 class TreeNode {
-     int val;
-     TreeNode left;
-     TreeNode right;
-     TreeNode() {}
-     TreeNode(int val) { this.val = val; }
-     TreeNode(int val, TreeNode left, TreeNode right) {
-         this.val = val;
-         this.left = left;
-         this.right = right;
-     }
- }
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 public class DiameterBT {
-    
-   static int diameter = 0;
 
-public static int height(TreeNode root) {
-    if (root == null) return 0;
-    // check children height
-    int lh = height(root.left);
-    int rh = height(root.right);
+    static int diameter = 0;
 
-    // update diameter
-    diameter = Math.max(diameter, lh + rh);
+    public static int height(TreeNode root) {
+        if (root == null)
+            return 0;
+        // check children height
+        int lh = height(root.left);
+        int rh = height(root.right);
 
-    // return height
-    return 1 + Math.max(lh, rh);
-}
+        // update diameter
+        diameter = Math.max(diameter, lh + rh);
 
-public static int diameterOfBinaryTree(TreeNode root) {
-    diameter = 0;
-    height(root);
-    return diameter;
-} 
-public static void main(String[] args) {
-    TreeNode root = new TreeNode(1);
-    root.left = new TreeNode(2);
-    root.right = new TreeNode(3);
-    root.left.left = new TreeNode(4);
-    root.left.right = new TreeNode(5);
+        // return height
+        return 1 + Math.max(lh, rh);
+    }
 
-    int ans = diameterOfBinaryTree(root);
-    System.out.println(ans);
-}
+    public static int diameterOfBinaryTree(TreeNode root) {
+        diameter = 0;
+        height(root);
+        return diameter;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        int ans = diameterOfBinaryTree(root);
+        System.out.println(ans);
+    }
 
 }
