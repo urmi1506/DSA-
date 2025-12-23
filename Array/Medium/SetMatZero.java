aa1337732ee89ebf2a65eeefaff62a27691077bc
urmi1506 <urmilaawasarmal@gmail.com>
2025-12-23 21:11:09 +0530

    Set Matrix Zeroes ---(Brute-force Sol)

--- Array/Medium/SetMatZero.java
+++ Array/Medium/SetMatZero.java
@@ -5,6 +5,7 @@ public class SetMatZero {
     public static void setZeroes(int[][] matrix) {
         int n=matrix.length;
         int m=matrix[0].length;
+        // traverse matrix to find zero
         for(int row=0; row<n; row++){
             for(int col=0; col<m; col++){
                 if(matrix[row][col]==0){
@@ -15,18 +16,21 @@ public class SetMatZero {
         }
     convert(matrix);
     }
+    // markRow as -1
     public static void markRow(int[][]matrix,int row){
         int m=matrix[0].length;
         for(int col=0; col<m; col++){
             if(matrix[row][col] !=0) matrix[row][col]=-1;
         }
     }
+     // markCol as -1
     public static void markCol(int[][]matrix,int col){
         int n=matrix.length;
         for(int row=0; row<n; row++){
             if(matrix[row][col]!=0) matrix[row][col]=-1;
         }
     }
+    // convert matrix -1 to 0
     public static void convert(int[][]matrix){
         int n=matrix.length;
         int m=matrix[0].length;
