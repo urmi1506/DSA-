92a48471fac6a7aa4ba1dc3c9367ce9bccf7d134
urmi1506 <urmilaawasarmal@gmail.com>
2026-01-05 18:45:13 +0530

    Children Sum in BT --(Brute-force)

--- Trees/ChildrenSumTree.java
+++ Trees/ChildrenSumTree.java
@@ -1,46 +1,53 @@
 package Trees;
 
 public class ChildrenSumTree {
-    static class Node{
-    int data;
-    Node left,right;
+    static class Node {
+        int data;
+        Node left, right;
 
-    Node(int key)
-    {
-        data = key;
-        left = right = null;
+        Node(int key) {
+            data = key;
+            left = right = null;
+        }
     }
-    }
-    static boolean res =true;
+
+    static boolean res = true;
+
     public boolean isSumProperty(Node root) {
-        int ans =dfs(root);
-        
-        if(ans == -1) return false;
-        
-    return ChildrenSumTree.res;
-        
+        int ans = dfs(root);
+
+        if (ans == -1)
+            return false;
+
+        return ChildrenSumTree.res;
+
     }
-    private int dfs(Node node){
-        if(node == null) return 0;
-        if(node.left == null && node.right == null) return node.data;
-        int left =dfs(node.left);
-        int right=dfs(node.right);
-        
-        if(left+right != node.data) ChildrenSumTree.res = false; 
-       
-    return node.data; 
+
+    private int dfs(Node node) {
+        if (node == null)
+            return 0;
+        if (node.left == null && node.right == null)
+            return node.data;
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+
+        if (left + right != node.data)
+            ChildrenSumTree.res = false;
+
+        return node.data;
     }
+
     public static void main(String[] args) {
         ChildrenSumTree tree = new ChildrenSumTree();
 
-    Node root = new Node(10);
-    root.left = new Node(8);
-    root.right = new Node(2);
-    root.left.left = new Node(3);
-    root.left.right = new Node(5);
+        Node root = new Node(10);
+        root.left = new Node(8);
+        root.right = new Node(2);
+        root.left.left = new Node(3);
+        root.left.right = new Node(5);
 
-    boolean result = tree.isSumProperty(root);
-    System.out.println(result); 
+        boolean result = tree.isSumProperty(root);
+        System.out.println(result);
     }
 
 }
