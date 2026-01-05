32f0b6f158fcbaff59fcff37d0b899c3a2e2e71f
urmi1506 <urmilaawasarmal@gmail.com>
2026-01-05 18:43:59 +0530

    Flatten BT to LL --(optimized-sol)

--- Trees/FlattenBinaryTree.java
+++ Trees/FlattenBinaryTree.java
@@ -1,37 +1,39 @@
 package Trees;
 
 public class FlattenBinaryTree {
-    static class TreeNode{
+    static class TreeNode {
         int val;
-        TreeNode left,right;
-        TreeNode(int val){
-            this.val=val;
-        }
-    }
-    public static void flatten(TreeNode root) {
-        TreeNode curr =root;
-        // check left-subtree
-        while(curr != null){
-            if(curr.left != null){
-               TreeNode prev = curr.left;
-            
-            // find rightmost node of left-subtree
-            while(prev.right != null){
-                prev =prev.right;
-            }
-            // attach rightmost node to curr node right
-            prev.right = curr.right;
-            // move left-subtree to right
-            curr.right = curr.left;
-            // nullify left
-            curr.left=null;
+        TreeNode left, right;
 
-        
-           } 
-         curr=curr.right;
+        TreeNode(int val) {
+            this.val = val;
         }
-        
     }
+
+    public static void flatten(TreeNode root) {
+        TreeNode curr = root;
+        // check left-subtree
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode prev = curr.left;
+
+                // find rightmost node of left-subtree
+                while (prev.right != null) {
+                    prev = prev.right;
+                }
+                // attach rightmost node to curr node right
+                prev.right = curr.right;
+                // move left-subtree to right
+                curr.right = curr.left;
+                // nullify left
+                curr.left = null;
+
+            }
+            curr = curr.right;
+        }
+
+    }
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
 
@@ -47,9 +49,9 @@ public class FlattenBinaryTree {
 
         TreeNode curr = root;
         while (curr != null) {
-        System.out.print(curr.val + "--> ");
-        curr = curr.right;
-    }
-    
+            System.out.print(curr.val + "--> ");
+            curr = curr.right;
+        }
+
     }
 }
