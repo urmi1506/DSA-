a3dafa588007362c86604fe8de33cdbafbe7c02f
urmi1506 <urmilaawasarmal@gmail.com>
2026-01-05 18:44:28 +0530

    Flatten BT to LL

--- Trees/FlattenBT.java
+++ Trees/FlattenBT.java
@@ -3,33 +3,39 @@ package Trees;
 import java.util.*;
 
 public class FlattenBT {
-    static class TreeNode{
+    static class TreeNode {
         int val;
-        TreeNode left,right;
-        TreeNode(int val){
-            this.val=val;
+        TreeNode left, right;
+
+        TreeNode(int val) {
+            this.val = val;
         }
     }
+
     public static void flatten(TreeNode root) {
-        if(root == null) return;
+        if (root == null)
+            return;
         // store preorder in list and give each node left as null
-        List<TreeNode>list =new ArrayList<>();
-        preorder(root ,list);
+        List<TreeNode> list = new ArrayList<>();
+        preorder(root, list);
         for (int i = 0; i < list.size() - 1; i++) {
             list.get(i).left = null;
             list.get(i).right = list.get(i + 1);
         }
-        
+
     }
+
     // preorder
-    private static void preorder(TreeNode node ,List<TreeNode> list){
-        if(node == null) return;
-       
+    private static void preorder(TreeNode node, List<TreeNode> list) {
+        if (node == null)
+            return;
+
         list.add(node);
-        preorder(node.left , list);
-        preorder(node.right ,list);
+        preorder(node.left, list);
+        preorder(node.right, list);
     }
-        public static void main(String[] args) {
+
+    public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
 
         root.left = new TreeNode(2);
@@ -41,6 +47,6 @@ public class FlattenBT {
         root.right.right = new TreeNode(6);
 
         flatten(root);
-    
+
     }
 }
