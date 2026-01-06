c4d8f5f74360bf014c9a172c9b93711917216b32
urmi1506 <urmilaawasarmal@gmail.com>
2026-01-06 19:41:58 +0530

    Validate BST

--- Trees/ValidateBST.java
+++ Trees/ValidateBST.java
@@ -2,33 +2,38 @@ package Trees;
 
 public class ValidateBST {
     static class Node {
-    int data;
-    Node left;
-    Node right;
+        int data;
+        Node left;
+        Node right;
 
-    public Node(int val) {
-        data = val;
-        left = null;
-        right = null;
+        public Node(int val) {
+            data = val;
+            left = null;
+            right = null;
+        }
     }
-  }
-  public static boolean isValidBST(TreeNode root) {
-        return isValid(root , Long.MIN_VALUE ,Long.MAX_VALUE);
-    }
-    private static boolean isValid(TreeNode node ,long mini ,long maxi){
-        if(node == null) return true;
 
-        if(node.val <=mini || node.val >= maxi) return false;
-
-    return isValid(node.left ,mini ,node.val)
-           && isValid(node.right , node.val ,maxi);
+    public static boolean isValidBST(TreeNode root) {
+        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
+
+    private static boolean isValid(TreeNode node, long mini, long maxi) {
+        if (node == null)
+            return true;
+
+        if (node.val <= mini || node.val >= maxi)
+            return false;
+
+        return isValid(node.left, mini, node.val)
+                && isValid(node.right, node.val, maxi);
+    }
+
     public static void main(String[] args) {
-        TreeNode root =new TreeNode(2);
-        root.left=new TreeNode(1);
-        root.right=new TreeNode(3);
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
 
-        boolean ans =isValidBST(root);
+        boolean ans = isValidBST(root);
         System.out.println(ans);
     }
 }
