84dc8b88c1a2e7eb26b63544763e2b6179b84778
urmi1506 <urmilaawasarmal@gmail.com>
2026-01-08 20:18:58 +0530

    Populating Next Right Pointers in Each Node ---(Optimized-sol)

--- Trees/NextRightPtr.java
+++ Trees/NextRightPtr.java
@@ -11,65 +11,67 @@ public class NextRightPtr {
             left = right = null;
         }
     }
+
     public static Node connect(Node root) {
-        Node curr = root ;
+        Node curr = root;
         // traverse level-level
-        while(curr != null){
+        while (curr != null) {
             Node dummy = new Node(0);
             // tail ---point to last node
             Node tail = dummy;
-            
+
             // traverse within level
-            while(curr != null){
-            
-            // check left --not null--append it
-            if(curr.left != null){
-                tail.next = curr.left;
-                tail = tail.next;
+            while (curr != null) {
+
+                // check left --not null--append it
+                if (curr.left != null) {
+                    tail.next = curr.left;
+                    tail = tail.next;
+                }
+                // check right --not null--append it
+                if (curr.right != null) {
+                    tail.next = curr.right;
+                    tail = tail.next;
+                }
+                curr = curr.next;
             }
-            // check right --not null--append it
-            if(curr.right != null){
-                tail.next = curr.right;
-                tail = tail.next;
-            }
-        curr=curr.next;
+            curr = dummy.next;
         }
-    curr=dummy.next;
+        return root;
     }
-    return root;
-   }
-   public static void printUsingNext(Node root) {
-    Node levelStart = root;
 
-    while (levelStart != null) {
-        Node curr = levelStart;
-        levelStart = null;
+    public static void printUsingNext(Node root) {
+        Node levelStart = root;
 
-        while (curr != null) {
-            System.out.print(curr.data + " -> ");
+        while (levelStart != null) {
+            Node curr = levelStart;
+            levelStart = null;
 
-            if (levelStart == null) {
-                if (curr.left != null) levelStart = curr.left;
-                else if (curr.right != null) levelStart = curr.right;
+            while (curr != null) {
+                System.out.print(curr.data + " -> ");
+
+                if (levelStart == null) {
+                    if (curr.left != null)
+                        levelStart = curr.left;
+                    else if (curr.right != null)
+                        levelStart = curr.right;
+                }
+                curr = curr.next;
             }
-            curr = curr.next;
+            System.out.println("null");
         }
-        System.out.println("null");
     }
-}
 
     public static void main(String[] args) {
         Node root = new Node(1);
-    root.left = new Node(2);
-    root.right = new Node(3);
-    root.left.left = new Node(4);
-    root.left.right = new Node(5);
-    root.right.left = new Node(6);
-    root.right.right = new Node(7);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
 
-    connect(root);
-    printUsingNext(root);
+        connect(root);
+        printUsingNext(root);
     }
 }
-
-
