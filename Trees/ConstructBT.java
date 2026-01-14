b79a67538f092441ad9215107e3e073b31e40f58
urmi1506 <urmilaawasarmal@gmail.com>
2026-01-14 21:04:57 +0530

    Construct Binary Tree from Preorder and Inorder Traversal

--- Trees/ConstructBT.java
+++ Trees/ConstructBT.java
@@ -13,49 +13,53 @@ public class ConstructBT {
             left = right = null;
         }
     }
+
     public static TreeNode buildTree(int[] preorder, int[] inorder) {
         // create map & add inorder to it
-        HashMap<Integer,Integer> inMap = new HashMap<>();
-        for(int i =0; i<inorder.length; i++){
-            inMap.put(inorder[i],i);
+        HashMap<Integer, Integer> inMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            inMap.put(inorder[i], i);
         }
         // build tree
-        TreeNode root = buildTree(preorder ,0,preorder.length -1,
-                                  inorder ,0, inorder.length-1 ,inMap);
+        TreeNode root = buildTree(preorder, 0, preorder.length - 1,
+                inorder, 0, inorder.length - 1, inMap);
 
-    return root;
+        return root;
     }
-    public static TreeNode buildTree(int[] preorder , int prestart ,int preEnd,
-                              int[] inorder ,int instart ,int inEnd ,Map<Integer,Integer>inMap){
 
-        if(prestart > preEnd || instart > inEnd) return null;
-        
-        // Find root through preorder 
+    public static TreeNode buildTree(int[] preorder, int prestart, int preEnd,
+            int[] inorder, int instart, int inEnd, Map<Integer, Integer> inMap) {
+
+        if (prestart > preEnd || instart > inEnd)
+            return null;
+
+        // Find root through preorder
         TreeNode root = new TreeNode(preorder[prestart]);
-        
+
         // check whr root +nt in inorder
         int inRoot = inMap.get(root.val);
         // check leftside of inorder
         int numLeft = inRoot - instart;
-        
-        // build LeftTree
-        root.left = buildTree(preorder , prestart + 1 ,prestart + numLeft ,
-                              inorder ,instart,inRoot-1 ,inMap);
-        // build rightTree
-        root.right =  buildTree(preorder , prestart + numLeft + 1 ,preEnd ,
-                              inorder ,inRoot+1 ,inEnd  ,inMap);      
 
-    return root;              
-                             
-     }
-     public static void main(String[] args) {
-        int preorder[] = {3,9,20,15,7};
-        int inorder[] = {9,3,15,20,7};
+        // build LeftTree
+        root.left = buildTree(preorder, prestart + 1, prestart + numLeft,
+                inorder, instart, inRoot - 1, inMap);
+        // build rightTree
+        root.right = buildTree(preorder, prestart + numLeft + 1, preEnd,
+                inorder, inRoot + 1, inEnd, inMap);
+
+        return root;
+
+    }
+
+    public static void main(String[] args) {
+        int preorder[] = { 3, 9, 20, 15, 7 };
+        int inorder[] = { 9, 3, 15, 20, 7 };
 
         TreeNode root = buildTree(preorder, inorder);
 
         printInorder(root);
-        }
+    }
 
     public static void printInorder(TreeNode root) {
         if (root == null) {
