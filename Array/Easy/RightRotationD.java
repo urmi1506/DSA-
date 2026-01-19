90dcdf1eb8314af895867b91629ac35201f39b8f
urmi1506 <urmilaawasarmal@gmail.com>
2026-01-19 21:40:19 +0530

    Right Rotation Array

--- Array/Easy/RightRotationD.java
+++ Array/Easy/RightRotationD.java
@@ -1,27 +1,29 @@
 package Easy;
 
 public class RightRotationD {
-    public static void reverse(int nums[] ,int s ,int e ){
-        while(s<e){
-            int temp =nums[s];
-            nums[s]=nums[e];
-            nums[e]=temp;
+    public static void reverse(int nums[], int s, int e) {
+        while (s < e) {
+            int temp = nums[s];
+            nums[s] = nums[e];
+            nums[e] = temp;
             s++;
             e--;
         }
 
     }
+
     public static void rotate(int[] nums, int k) {
-        int n=nums.length;
+        int n = nums.length;
         // reverse whole arr
-        reverse(nums, 0, n-1);
-        //  reverse first k part
-        reverse(nums,0,k-1);
+        reverse(nums, 0, n - 1);
+        // reverse first k part
+        reverse(nums, 0, k - 1);
         // reverse remaining k-1 part
-        reverse(nums, k, n-1);
+        reverse(nums, k, n - 1);
     }
+
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
         int k = 3;
 
         rotate(nums, k);
