59048d9451eaa3ed9b10870c7afd0c095262f6e9
urmi1506 <urmilaawasarmal@gmail.com>
2026-01-26 21:28:09 +0530

    Minimum Absolute Difference

---
+++ Array/Easy/MinabsDiff.java
@@ -0,0 +1,38 @@
+package Easy;
+
+import java.util.*;
+
+public class MinabsDiff {
+
+    public static List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        int minDiff = Integer.MAX_VALUE;
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 1; i < n; i++) {
+            int diff = arr[i] - arr[i - 1];
+
+            if (diff < minDiff) {
+                minDiff = diff;
+                res = new ArrayList<>();
+                res.add(Arrays.asList(arr[i - 1], arr[i]));
+            } 
+            else if (diff == minDiff) {
+                res.add(Arrays.asList(arr[i - 1], arr[i]));
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 1, 3};
+
+        List<List<Integer>> res = minimumAbsDifference(arr);
+
+        
+        for (List<Integer> list : res) {
+            System.out.println(list);
+        }
+    }
+}
