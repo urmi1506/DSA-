0b9b171ce785d76849fcd5c91de90240f5dd907c
urmi1506 <urmilaawasarmal@gmail.com>
2026-02-01 12:44:16 +0530

    Merge Sorted Array

--- Sorting/MergeSortArr2.java
+++ Sorting/MergeSortArr2.java
@@ -12,6 +12,7 @@ public class MergeSortArr2 {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = 0, j = 0, k = 0;
         int[] mergedArray = new int[m + n];
+        
         while (i < m && j < n) {
             if (nums1[i] < nums2[j]) {
                 mergedArray[k++] = nums1[i++];
