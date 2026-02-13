94507d2da20553bd7aef24dab182f961a0a7fa44
urmi1506 <urmilaawasarmal@gmail.com>
2026-02-13 21:57:58 +0530

    Remove Duplicates from Array (Optimized-sol)

--- Array/Easy/RemDuplicates.java
+++ Array/Easy/RemDuplicates.java
@@ -1,7 +1,5 @@
 package Easy;
-// Remove Duplicate Element From Sorted Array
 public class RemDuplicates {
-    // in this logic we skip duplicate and add unique ele
     public static int removeDuplicates(int[] arr){
         int n = arr.length;
         // track unique ele
@@ -12,13 +10,12 @@ public class RemDuplicates {
                 arr[j] = arr[i]; //move unique ele to nxt pos
             }
         }
-    return j+1; // give total unique ele
+    return j+1; 
     }
     public static void main(String[] args) {
         int[] arr={3,3,5,6};
         int newLen=removeDuplicates(arr);
         System.out.println();
-        // if loop upto old arr length it provide last val as duplicate
         for (int i = 0; i < newLen; i++) {
             System.out.println(arr[i]);
         }
