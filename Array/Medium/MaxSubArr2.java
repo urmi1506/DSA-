59dd968176e54672942b5560234dad5b1b99560f
urmi1506 <urmilaawasarmal@gmail.com>
2026-02-14 11:27:54 +0530

    Kadanes Algo (optimized-sol)

--- Array/Medium/MaxSubArr2.java
+++ Array/Medium/MaxSubArr2.java
@@ -6,8 +6,8 @@ public class MaxSubArr2 {
     public static int maxSubArray(int[] nums) {
        int n = nums.length;
 
-       int sum = 0;
        int maxi = Integer.MIN_VALUE;
+       int sum = 0;
 
        for (int i = 0; i < n; i++) {
            sum += nums[i];
