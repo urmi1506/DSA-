66b1ac172566f008a329e2fc5725736cef9da902
urmi1506 <urmilaawasarmal@gmail.com>
2026-03-09 19:06:17 +0530

    Implement Stack Using Array

--- Stack/ImplementUsingArr.java
+++ Stack/ImplementUsingArr.java
@@ -1,52 +1,59 @@
-
 public class ImplementUsingArr {
-    static int []arr;
-    static int top;
-    public ImplementUsingArr(){
-       arr=new int [arr.length];
-       top=-1;
+
+    private int[] arr;
+    private int top;
+
+    // constructor
+    public ImplementUsingArr(int size) {
+        arr = new int[size];
+        top = -1;
     }
-    static void push(int x){
-        if(arr.length-top>1){
-           arr[++top]=x;
+
+    // push
+    public void push(int x) {
+        if (top == arr.length - 1) {
+            System.out.println("Stack Overflow");
+            return;
         }
-        else{
-            System.out.println("stack overflow");
-        }
+        arr[++top] = x;
     }
-    static int pop(){
-        if(top>=0){
-            int ans=arr[top--];
-            return ans; 
-        }
-        else{
-            System.out.println("Stack underflow");
+
+    // pop
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
             return -1;
         }
+        return arr[top--];
     }
-    static int peek(){
-        if(top>=0){
-            int ans=arr[top];
-            return ans;
-        }
-        else{
-            System.out.println("Stack empty");
+
+    // peek
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is Empty");
             return -1;
         }
+        return arr[top];
     }
+
+    // check empty
     public boolean isEmpty() {
-        return (top == -1);  
+        return top == -1;
     }
+
     public static void main(String[] args) {
-        StackImplement stack = new StackImplement(5); 
+
+        ImplementUsingArr stack = new ImplementUsingArr(5);
+
         stack.push(10);
         stack.push(20);
         stack.push(30);
 
         System.out.println("Top element: " + stack.peek());
+
         stack.pop();
+
         System.out.println("Top element after pop: " + stack.peek());
         System.out.println("Is stack empty? " + stack.isEmpty());
     }
-
 }
