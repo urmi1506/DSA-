3ee1f0408c98086f9d3b129b18df58c3b5ecf7fa
urmi1506 <urmilaawasarmal@gmail.com>
2026-03-17 22:01:48 +0530

    Sum Tree

--- Trees/SumTree.java
+++ Trees/SumTree.java
@@ -6,16 +6,7 @@ public class SumTree {
         int data;
         TreeNode left;
         TreeNode right;
-        TreeNode() {
-            this.data = 0;
-            this.left = null;
-            this.right = null;
-        }
-        TreeNode(int data) {
-            this.data = data;
-            this.left = null;
-            this.right = null;
-        }
+        
         TreeNode(int data, TreeNode left, TreeNode right) {
             this.data = data;
             this.left = left;
