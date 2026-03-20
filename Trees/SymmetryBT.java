0bc77fb99b47b6d4d8e5b0582a2e28e66c704ae1
urmi1506 <urmilaawasarmal@gmail.com>
2026-03-20 23:37:57 +0530

    Symmetry Binary Tree

--- Trees/SymmetryBT.java
+++ Trees/SymmetryBT.java
@@ -1,16 +1,21 @@
 package Trees;
+
 public class SymmetryBT {
     public static boolean isSymmetric(TreeNode root) {
-        return isMirror(root.left ,root.right);
+        return isMirror(root.left, root.right);
     }
-    private static boolean isMirror(TreeNode a ,TreeNode b){
-       // both node null
-       if(a == null && b == null) return true;
-       //one node null
-       if(a == null || b == null) return false;
-       //check node are mirror or not
-       return a.val == b.val && isMirror(a.right ,b.left) && isMirror(a.left ,b.right);
+
+    private static boolean isMirror(TreeNode a, TreeNode b) {
+        // both node null
+        if (a == null && b == null)
+            return true;
+        // one node null
+        if (a == null || b == null)
+            return false;
+        // check node are mirror or not
+        return a.val == b.val && isMirror(a.right, b.left) && isMirror(a.left, b.right);
     }
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
