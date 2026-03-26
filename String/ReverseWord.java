3c6a8cab15e4ee83abdfcb7a025f32606af2066f
urmi1506 <urmilaawasarmal@gmail.com>
2026-03-26 21:57:40 +0530

    Reverse Words in a String

---
+++ String/ReverseWord.java
@@ -0,0 +1,23 @@
+package String;
+public class ReverseWord{
+    public static String reverseWords(String s) {
+        // Trim the string to remove leading & trailing spaces
+        // Split using "\\s+" to handle multiple spaces as a single separator
+        String[] words = s.trim().split("\\s+");
+        StringBuilder reversed = new StringBuilder();
+
+        for(int i = words.length -1; i>=0; i--){
+            reversed.append(words[i]);
+
+            if(i != 0) reversed.append(" ");
+        }
+    return reversed.toString();
+    }
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+        String ans = reverseWords(s);
+        System.out.println(ans);
+
+    }
+
+}
