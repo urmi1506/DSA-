5498d257f2f217cb5ea41021b38e4c891d5d1230
urmi1506 <urmilaawasarmal@gmail.com>
2026-03-27 20:17:00 +0530

    Diameter Of Binary Tree

--- Trees/DiameterBT.java
+++ Trees/DiameterBT.java
@@ -5,17 +5,10 @@ class TreeNode {
     TreeNode left;
     TreeNode right;
 
-    TreeNode() {
-    }
-
     TreeNode(int val) {
         this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+        left =null;
+        right =null;
     }
 }
 
