30305a3cee612e1479c77c3154517e92067939eb
urmi1506 <urmilaawasarmal@gmail.com>
2026-04-05 19:18:08 +0530

    Longest Common Prefix

--- String/LongestCommonPrefix.java
+++ String/LongestCommonPrefix.java
@@ -8,9 +8,7 @@ public class LongestCommonPrefix {
 
         for (int i = 0; i < strs[0].length(); i++) {
             for (String s : strs) {
-                if (i == s.length() || s.charAt(i) != strs[0].charAt(i)) {
-                    return res.toString();
-                }
+                if (i == s.length() || s.charAt(i) != strs[0].charAt(i)) return res.toString();
             }
             res.append(strs[0].charAt(i));
         }
