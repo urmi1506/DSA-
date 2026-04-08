3c7d1f6ba7ba5e11e759718ca4f9559945dc7a30
urmi1506 <urmilaawasarmal@gmail.com>
2026-04-08 14:17:04 +0530

    Largest Element -- Brute Force

--- Array/Easy/LargeEle.java
+++ Array/Easy/LargeEle.java
@@ -3,6 +3,7 @@ public class LargeEle {
     public static int largestElement(int[] nums) {
         int n = nums.length;
         int maxi = nums[0];
+        if( n == 1) return nums[0];
         for(int i =1 ; i < n-1; i++){
             if (nums[i] > nums [maxi]){
                 maxi= nums[i];
