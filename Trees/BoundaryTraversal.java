00f26dca078164ba1b52639d75f6f0e65ab097d3
urmi1506 <urmilaawasarmal@gmail.com>
2026-04-09 23:44:58 +0530

    Boundary Order Traversal --Optimized-sol

--- Trees/BoundaryTraversal.java
+++ Trees/BoundaryTraversal.java
@@ -14,39 +14,48 @@ public class BoundaryTraversal {
     }
 
     static ArrayList<Integer> l = new ArrayList<>();
-    //Left -- Leaf Node -- Right 
-    static ArrayList<Integer> boundaryTraversal(Node root) {
-        if (root == null)
-            return l;
 
+    static ArrayList<Integer> boundaryTraversal(Node root) {
+        // Empty root --return empty list
+        if (root == null) return l;
+        
+        // only one node --root is leaf
         if (root.left == null && root.right == null) {
             l.add(root.data);
             return l;
         }
-
+        
+        // add root node
         l.add(root.data);
 
+        // check left subtree
         if (root.left != null) {
             findleft(root.left);
         }
-
+        
+        // check leaf node
         leafNode(root);
-
+        
+        // check right subtree
         if (root.right != null) {
             findright(root.right);
         }
-
+        
+        // return final traversal
         return l;
     }
 
     static void findleft(Node root) {
-        if (root == null)
-            return;
-        if (root.left == null && root.right == null)
-            return;
-
+        // base case
+        if (root == null) return;
+        
+        // leaf node --skip
+        if (root.left == null && root.right == null) return;
+        
+        // add curr node
         l.add(root.data);
-
+        
+        // check left --> right
         if (root.left != null) {
             findleft(root.left);
         } else {
@@ -55,32 +64,35 @@ public class BoundaryTraversal {
     }
 
     static void leafNode(Node root) {
-        if (root == null)
-            return;
-
+        // base case
+        if (root == null) return;
+        
+        // leaf node --add it
         if (root.left == null && root.right == null) {
             l.add(root.data);
             return;
         }
-
+        // check left ---> right
         leafNode(root.left);
         leafNode(root.right);
     }
-
+    
     static void findright(Node root) {
-        if (root == null)
-            return;
-        if (root.left == null && root.right == null)
-            return;
-
+        // base case
+        if (root == null) return;
+        // leaf node skip it
+        if (root.left == null && root.right == null) return;
+        
+        // check right ---> left
         if (root.right != null) {
             findright(root.right);
         } else {
             findright(root.left);
         }
-
+        // add after recursion ---bottom to top
         l.add(root.data);
     }
+
 
     public static void main(String[] args) {
         Node root = new Node(20);
