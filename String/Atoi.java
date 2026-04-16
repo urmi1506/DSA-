56d5c63c80657b1b29bd123a4fb6089aae1b23ef
urmi1506 <urmilaawasarmal@gmail.com>
2026-04-16 22:19:37 +0530

    ATOI

---
+++ String/Atoi.java
@@ -0,0 +1,45 @@
+package String;
+
+class Atoi {
+      public static int myAtoi(String s) {
+        
+        s = s.trim();
+        if (s.length() == 0) {
+            return 0;
+        }
+        int cnt = 0;
+        int sign = 1; 
+        
+        int i = 0;
+        if (s.charAt(0) == '-') {
+            sign = -1;
+            i = 1;
+        } else if (s.charAt(0) == '+') {
+            i = 1;
+        }
+        
+        for (; i < s.length(); i++) {
+            if (!Character.isDigit(s.charAt(i))) {
+                break;
+            }
+            int n = s.charAt(i) - '0';
+            
+            if (cnt > (Integer.MAX_VALUE - n) / 10) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            
+            cnt = cnt * 10 + n;
+        }
+        
+        cnt = cnt * sign;
+        
+        return cnt;
+    }
+    public static void main(String[] args) {
+        String s = "42";
+        int ans = myAtoi(s);
+        System.out.println(ans);
+    }
+
+    
+}
