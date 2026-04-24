c9c6052f468a3fd54a96d87dc2acbb461d66ae8a
urmi1506 <urmilaawasarmal@gmail.com>
2026-04-24 23:43:17 +0530

    2833. Furthest Point From Origin

---
+++ Array/Easy/FurtherestDist.java
@@ -0,0 +1,20 @@
+package Easy;
+
+public class FurtherestDist {
+    public static int furthestDistanceFromOrigin(String moves) {
+        int left = 0, right = 0, blanks = 0;
+
+        for (char c : moves.toCharArray()) {
+            if (c == 'L') left++;
+            else if (c == 'R') right++;
+            else blanks++;
+        }
+
+        return Math.abs(left - right) + blanks;
+    }
+    public static void main(String[] args) {
+        String moves = "L_RL__R";
+        int furtherest_dist = furthestDistanceFromOrigin(moves);
+        System.out.println(furtherest_dist);
+    }
+}
