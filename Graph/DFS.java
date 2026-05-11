79e99e18b0409b769467d6de4dcd71a85e39f4bb
urmi1506 <urmilaawasarmal@gmail.com>
2026-05-11 14:49:26 +0530

    DFS of Graph

--- Graph/DFS.java
+++ Graph/DFS.java
@@ -3,24 +3,30 @@ import java.util.*;
 
 public class DFS {
     
-    static void dfsRec(ArrayList<ArrayList<Integer>> adj,
-           boolean[] visited, int s, ArrayList<Integer> res)
-    {
-        visited[s] = true;
-        res.add(s);
-
-        //check neighbours
-        for (int i : adj.get(s)) {
-            if (!visited[i]) {
-                dfsRec(adj, visited, i, res);
+    public static ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        int size = adj.size();
+        // initialized arraylist ---> store dfs traversal
+        ArrayList<Integer> dfs = new ArrayList<>();
+        // initialized boolean array --> store visisted node
+        boolean[] visited = new boolean[size];
+        // traverse dfs recursively
+        dfsRecursive(adj , 0, visited ,dfs);
+        
+    return dfs;
+    }
+    private static void dfsRecursive(ArrayList<ArrayList<Integer>> adj , int node 
+    ,boolean []visited ,ArrayList<Integer>dfs){
+        // viisted node ,mark as true & add to dfs
+        visited[node]= true;
+        dfs.add(node);
+        
+        // check neighbour --> if not visited --traverse dfs Recursively
+        for(int current : adj.get(node)){
+            if(!visited[current]){
+                dfsRecursive(adj ,current ,visited ,dfs);
             }
         }
-    }
-    static ArrayList<Integer>dfs(ArrayList<ArrayList<Integer>> adj) {
-        boolean[] visited = new boolean[adj.size()];
-        ArrayList<Integer> res = new ArrayList<>();
-        dfsRec(adj, visited, 0, res);
-        return res;
     }
     public static void main(String[] args) {
         int n = 5; 
