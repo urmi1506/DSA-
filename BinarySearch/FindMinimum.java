108791741929f4e24c8781dcb203f41ae38158f4
urmi1506 <urmilaawasarmal@gmail.com>
2026-05-15 14:01:38 +0530

    153. Find Minimum in Rotated Sorted Array --optimized-sol

--- BinarySearch/FindMinimum.java
+++ BinarySearch/FindMinimum.java
@@ -20,7 +20,7 @@ public class FindMinimum {
         
     }
     public static void main(String[] args) {
-        int nums[]={3,4,5,1,2};
+        int nums[]={4,5,6,7,0,1,2};
         System.out.println(findMin(nums));
     }
 
