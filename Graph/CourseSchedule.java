5d2004e399037dee427c0dd831a318122a355a8c
urmi1506 <urmilaawasarmal@gmail.com>
2026-06-02 13:01:05 +0530

    207. Course Schedule

--- Graph/CourseSchedule.java
+++ Graph/CourseSchedule.java
@@ -7,20 +7,21 @@ import java.util.Queue;
 public class CourseSchedule {
     public static boolean canFinish(int numCourses, int[][] prerequisites) {
 
-        // Create adjacency list
+        // Create adjacency list --->check dependency
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for (int i = 0; i < numCourses; i++) {
             adj.add(new ArrayList<>());
         }
 
-        // Build the graph 
+        // Build the graph -->know the edges (dependency)
         for (int i = 0; i < prerequisites.length; i++) {
             int a = prerequisites[i][0];
             int b = prerequisites[i][1];
+            // b must completed before a
             adj.get(b).add(a);
         }
 
-        //Calculate indegree
+        //Calculate indegree --> know starting point 
         int[] indegree = new int[numCourses];
         for (int i = 0; i < numCourses; i++) {
             for (int it : adj.get(i)) {
@@ -28,7 +29,7 @@ public class CourseSchedule {
             }
         }
 
-        //Add nodes with indegree 0
+        //Add nodes with indegree 0 -->their no prerequisites → can be taken first
         Queue<Integer> q = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
             if (indegree[i] == 0) {
@@ -37,20 +38,23 @@ public class CourseSchedule {
         }
 
         // BFS 
-        int cnt = 0;
+        int cnt = 0; //-->check we proceed all courses or not (cycle or not)
 
         while (!q.isEmpty()) {
             int node = q.poll();
+            // 1 course completed
             cnt++;
 
             for (int it : adj.get(node)) {
+                // As course completed need to remove dependency
                 indegree[it]--;
+                // check other course which dont have prerequisite
                 if (indegree[it] == 0) {
                     q.add(it);
                 }
             }
         }
-
+        // All courses proceed (no cycle)
         return cnt == numCourses;
     }
   
