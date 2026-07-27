c0a8b907867a9fdba8dd6f18278301f850ba21c5
urmi1506 <urmilaawasarmal@gmail.com>
2026-07-27 19:56:19 +0530

    169. Majority Element --optimized-sol

--- Array/Hard/MajorityEle.java
+++ Array/Hard/MajorityEle.java
@@ -2,13 +2,34 @@ package Hard;
 
 public class MajorityEle {
     public static int majorityElement(int[]nums){
-        int majority=0;
-        int cnt=0;
-        for (int num : nums) {
-            if(cnt==0) majority=num;
-            cnt += (num == majority)?1:-1;
+        int n = nums.length;
+        
+        // Find majority 
+        int majority = 0;
+        int cnt =0;
+        for(int num : nums){
+            if(cnt == 0){
+                majority =num;
+            }
+            if(num == majority){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
         }
-    return majority;
+
+        // verify majority element 
+        cnt =0;
+        for(int num : nums){
+            if(num == majority){
+                cnt ++;
+            }
+        }
+        if(cnt > n/2){
+            return majority;
+        }
+    return -1;
     }
     public static void main(String[] args) {
         int[]nums={3,2,3};
