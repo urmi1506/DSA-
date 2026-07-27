eb762c966a4d52ccf83d2a4d01c6e8f9656b9ced
urmi1506 <urmilaawasarmal@gmail.com>
2026-07-27 19:49:39 +0530

    169. Majority Element --Brute Force

--- Array/Hard/Majority.java
+++ Array/Hard/Majority.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 public class Majority {
     public static int majorityElelment(int []nums){
         int n=nums.length;
+         // work only majority element is present 
+        // Fail cond ---> 1,2,3  or 1,1,2,2 (no majority)
         Arrays.sort(nums);
         return nums[n/2];
     }
