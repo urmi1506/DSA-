dad94fea87e682685e4ae7533987cc512cfd4217
urmi1506 <urmilaawasarmal@gmail.com>
2026-07-27 20:49:21 +0530

    128. Longest Consecutive Sequence ---optimized-sol

--- Array/Medium/LongestConsecutive.java
+++ Array/Medium/LongestConsecutive.java
@@ -5,9 +5,9 @@ import java.util.HashSet;
 public class LongestConsecutive {
     public static int longestConsecutive(int[] nums) {
         int n = nums.length;
-
+        // Edge case --[]
         if (n == 0) return 0;
-
+        // handle duplicates
         HashSet<Integer> set = new HashSet<>();
 
         for (int num : nums) {
@@ -17,12 +17,12 @@ public class LongestConsecutive {
         int longest = 1;
 
         for (int num : set) {
-
+            // check starting point
             if (!set.contains(num - 1)) {
 
                 int currNum = num;
                 int currCnt = 1;
-
+                // check consecutive
                 while (set.contains(currNum + 1)) {
                     currNum++;
                     currCnt++;
