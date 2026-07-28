54cc9d3bc810142145c1c381e36160458c6b7da0
urmi1506 <urmilaawasarmal@gmail.com>
2026-07-28 14:36:24 +0530

    25. Reverse Nodes in k-Group

--- LinkedList/ReverseKNode.java
+++ LinkedList/ReverseKNode.java
@@ -11,6 +11,7 @@ class ListNode{
 }
 public class ReverseKNode{
     public static ListNode reverseKGroup(ListNode head, int k) {
+        // Edge case
         if(head == null)
            return null;
 
